666732f77379c4d30cd708d837d39db6c2830eef
Christian Oller <christianoller8@gmail.com>
2022-09-02 11:52:02 +0200

    acabar app

--- script-sp-tipificacion.js
+++ script-sp-tipificacion.js
@@ -7,8 +7,40 @@ $( document ).ready(function() {
       window.location.href = './index-login-tipificacion.html';
       
     }
+    let sessionUser = $('#user').text(data[0].name)
+    
 });
 
+function logout() {
+  sessionStorage.removeItem('user');
+  window.location.href = './index-login-tipificacion.html';
+}
+
+function startTime() {
+  var today = new Date();
+  var day = today.getDate();
+  var month = today.getMonth()+1;
+  var year = today.getFullYear();
+
+  day = checkTime(day)
+  month =checkTime(month)
+  let date = $('#fecha').text(day + "/" + month + "/" + year) 
+
+  var hr = today.getHours();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+  //Add a zero in front of numbers<10
+  min = checkTime(min);
+  sec = checkTime(sec);
+  document.getElementById("hora").innerHTML = hr + " : " + min + " : " + sec;
+  setTimeout(function(){ startTime() }, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+      i = "0" + i;
+  }
+  return i;
+}
 
 $('#submit').click(submitInfo)
 
@@ -17,25 +49,15 @@ function onlyOne(checkbox) {
   checkboxes.forEach((item) => {
     if (item !== checkbox) {
       item.checked = false
-      
+
       if ($('input[name="check"]').attr('checked', true)) {
-        let checkboxes2 = $('input[name="check"]:checked').attr('id')        
+        let checkboxes2 = $('input[name="check"]:checked').attr('id')
         return checkboxes2
       }
 
     }
-    
+
 })
- 
-
-/*
-  let checkboxes = $('input[name="check"]:checked').attr('id')
-
-console.log(checkboxes);
-*/
-  
-  
-  
 }
 
 function submitInfo(event){
@@ -68,35 +90,80 @@ function submitInfo(event){
     
     alert('No has seleccionado ningun tipo de consulta')
   }
-
-
-
 }
 
 
+function output(){
+  let array =[
+    {
+      id:1,
+      value:'name'
+    },
+    {
+      id:2,
+      value:'password'
+    }
+  ]
+  axios.get(`https://tipificaciones-backend.herokuapp.com/api/tipificaciones`)
+  .then(response => {
+    const tipificaciones = response.data;
+    downloadExcel(tipificaciones)
+
+  })
+  .catch(error => console.error(error));
+
+  
+}
 
 
-/*
-$('input[type="checkbox"]').on('change', function() {
-  $(this).siblings('input[type="checkbox"]').prop('checked', false);
-});
-
-$("input:checkbox").on('click', function() {
-
-  var $box = $(this);
-  if ($box.is(":checked")) {
-
-    var group = "input:checkbox[name='" + $box.attr("name") + "']";
-   
-    $(group).prop("checked", false);
-    $box.prop("checked", true);
-  } else {
-    $box.prop("checked", false);
+function downloadExcel(tipificacionesExcel){
+  const fileName = 'output.xlsx';
+  const ws = XLSX.utils.json_to_sheet(tipificacionesExcel);
+  const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'output');
+    XLSX.writeFile(wb, fileName);
   }
-});
+
+  /*
+  $(document).ready(function() {
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        button: [
+            'excel'
+        ]
+    } );
+} );
+
 */
+/*
+var userTest = localStorage.getItem("user");
+  if (userTest == null) {
+    window.location.href = './index-login-tipificacion'
+  }
 
+  function loadUser() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://tipificaciones-backend.herokuapp.com/api/tipificacion?user_id=${userId}&marker_option=${checkboxes2}&language=${idioma}");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.setRequestHeader("Authorization", "Bearer "+jwt);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      const objects = JSON.parse(this.responseText);
+      if (objects["status"] == "ok") {
+        const user = objects["user"]
+        document.getElementById("fname").innerHTML = user["fname"];
+        document.getElementById("username").innerHTML = password["password"];
+      }
+    }
+  };
+}
 
+loadUser();
 
-
+function logout() {
+  localStorage.removeItem("userTest");
+  window.location.href = './login.html'
+}
+*/
 
